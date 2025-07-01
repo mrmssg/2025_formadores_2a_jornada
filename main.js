a201d4a0adb79a6c6287b6611e3393e1edf520ad
Mauro S S Goncalves <133293974+mrmssg@users.noreply.github.com>
2025-07-01 17:44:27 +0000

    005

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
-let nomeUsuario = "";
-let elemento = document.querySelector("#nome-do-usuario");
-
-while (nomeUsuario =="") {
-    nomeUsuario = prompt("Qual é o seu nome?");
+function inserirNome(){
+   let nomeUsuario = prompt("Qual o seu nome");
+   let elemento = document.querySelector("#nome-usuario");
+   elemento.textContent = nomeUsuario;
 }
 
+inserirNome();
 
-if(nomeUsuario==null){
-    elemento.textContent="seja muito bem-vindo!";
-} else{
-    elemento.textContent = nomeUsuario;
-}
+let linguagens =["JavaScript","Python","C","C++"]
+console.log(linguagens[0]) // “JavaScript”
+console.log(linguagens[1])  // “Python”
+console.log(linguagens[2])  // “C”
+console.log(linguagens[3])  // “C++”
+
+const item = document.querySelector("#lista");
+item.textContent = linguagens[0];
+item.textContent = linguagens[1];
+item.textContent = linguagens[2];
+item.textContent = linguagens[3];
